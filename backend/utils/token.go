2ffea0f24fde5cbe400473c274f85bc2e05057eb
ProgrammerPeasant <poland-fish@yandex.ru>
2025-02-20 09:42:20 +0300

    updated token.go

--- backend/utils/token.go
+++ backend/utils/token.go
@@ -3,12 +3,22 @@ package utils
 import (
 	"fmt"
 	"github.com/ProgrammerPeasant/order-control/models"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-var jwtSecret = []byte("4c89213f58c5fa8b9ad4827f869a8b926e8b4c73ca75c1391cd150d375519cd33bc72dbe622a47310dc57b49faf6537214e0266b305b05cc704b77a6a7533d445a2b9218247973a8ebbec220fcc1b3bc1417377b33d33800efe40f0fee2bc1f714474bfd620bfb1dcfc1ac25b2badef3cf1a360c11fc103b006f58323eb102b3bdce7c8b3a9b4658fd3506f921df803aec3547ac38fce044a24b968202371a76ed6fa168e6ecde7917d8cf4e80876fba50667622bb5a6e86d6ea4b15316cc820bb076870aed751acc64f2faec605bda1920d183364f35b4826f5e800647f8f9b5fdace980806bff895bc5cc6c350eea9c2a587649c7eb840d45dac635dec4cc3") // Обычно храним в env
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: No .env file found")
+	}
+}
+
+var jwtSecret = []byte(os.Getenv("JWTOKEN")) // Обычно храним в env
 
 type Claims struct {
 	UserID uint        `json:"user_id"`
