8013b88708b70bc8ca70e48cad1772747f03a335
Pysin Artem Aleksandrovich <144371806+skkkystyle@users.noreply.github.com>
2025-04-10 10:19:29 +0300

    routes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {AuthProvider} from "./Utils/AuthProvider";
+import PrivateRoute from "./Utils/PrivateRoute";
+import RoleRoute from "./Utils/RoleRoute";
 
 import StartPage from "./pages/StartPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -10,6 +12,8 @@ import SettingsPage from "./pages/SettingsPage";
 import AdminDashboardPage from "./pages/AdminDashboardPage/AdminDashboardPage";
 import ManagerDashboardPage from "./pages/ManagerDashboardPage";
 import EstimateViewPage from "./pages/EstimateViewPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AccessDeniedPage from "./pages/AccessDeniedPage";
 
 
 function App() {
@@ -20,11 +24,14 @@ function App() {
                     <Route path="/" element={<StartPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/clientdashboard" element={<ClientDashboardPage />} />
-                    <Route path="/settings" element={<SettingsPage />} />
-                    <Route path="/admin" element={<AdminDashboardPage />} />
-                    <Route path="/managerdashboard" element={<ManagerDashboardPage />} />
-                    <Route path="/estimateview/:estimateId" element={<EstimateViewPage />} />
+
+                    <Route path="/clientdashboard" element={<PrivateRoute><RoleRoute allowedRoles={["USER", "ADMIN"]}><ClientDashboardPage /></RoleRoute></PrivateRoute>} />
+                    <Route path="/settings" element={<PrivateRoute><RoleRoute allowedRoles={["USER", "MANAGER", "ADMIN"]}><SettingsPage /></RoleRoute></PrivateRoute>} />
+                    <Route path="/admin" element={<PrivateRoute><RoleRoute allowedRoles={["ADMIN"]}><AdminDashboardPage /></RoleRoute></PrivateRoute>} />
+                    <Route path="/managerdashboard" element={<PrivateRoute><RoleRoute allowedRoles={["MANAGER", "ADMIN"]}></RoleRoute><ManagerDashboardPage /></PrivateRoute>} />
+                    <Route path="/estimateview/:estimateId" element={<PrivateRoute><RoleRoute allowedRoles={["USER", "MANAGER", "ADMIN"]}><EstimateViewPage /></RoleRoute></PrivateRoute>} />
+                    <Route path="*" element={<NotFoundPage />} />
+                    <Route path="/accessdenied" element={<AccessDeniedPage />} />
                 </Routes>
             </Router>
         </AuthProvider>
---
+++ frontend/src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,12 @@
+import styles from "./NotFoundPage.module.css"
+import React from "react";
+
+const NotFoundPage = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.text}>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage;
